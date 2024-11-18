d92aff5ee45958e6872891f2618bc61b852a7764
Kusa Rabuma <rabumamotuma5@gmail.com>
2024-11-18 19:56:35 +0300

    My second day journey

--- src/Components/Top/Header.jsx
+++ src/Components/Top/Header.jsx
@@ -8,21 +8,25 @@ function Header() {
             <a href='index.html' className='nav-logo'>Motuma</a>
             <div className="nav-menu">
                 <ul className="nav-list grid">
+
                     <li className="nav-item">
-                        <a href="#home" className="nav-link active-link">
+                        <a href="#home" className="nav-link">
                             <i class="uil uil-house-user"></i> Home
                         </a>
                     </li>
+
                     <li className="nav-item">
                         <a href="#about" className="nav-link ">
                             <i className="uil uil-user nav__icon"></i> About
                         </a>
                     </li>
+
                     <li className="nav-item">
                         <a href="#skills" className="nav-link">
                             <i className="uil uil-file-alt nav__icon"></i>Skills
                         </a>
                     </li>
+
                     <li className="nav-item">
                         <a href="#services" className="nav-link">
                             <i className="uil uil-briefcase-alt nav__icon"></i>
@@ -36,6 +40,7 @@ function Header() {
                             Portfolio
                         </a>
                     </li>
+
                     <li className="nav-item">
                         <a href="#contact" className="nav-link">
                             <i className="uil uil-message nav__icon"></i>
